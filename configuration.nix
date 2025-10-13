d89450221610e532349119ef82d618278f671071
BleSSSeDDD <120226159+BleSSSeDDD@users.noreply.github.com>
2025-10-13 19:06:40 +0300

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+  system.stateVersion = "25.05";
+
+  boot.loader.systemd-boot.enable = true;
+  networking.hostName = "nixos-dev";
+  networking.networkmanager.enable = true;
+
+  users.users.dev = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    initialPassword = "12345678";
+  };
+
+  environment.systemPackages = with pkgs; [
+    net-tools iproute2 bind dnsutils
+    vim wget curl git htop tree
+    file pciutils usbutils
+    virtualbox-guest-additions
+  ];
+
+  virtualisation.virtualbox.guest = {
+    enable = true;
+    x11 = true;
+  };
+
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      lightdm.enable = true; 
+      autoLogin = {
+        enable = true;
+        user = "dev";
+      };
+    };
+    desktopManager = {
+      lxde.enable = true;
+    };
+    videoDrivers = [ "virtualbox" "modesetting" ];
+  };
+
+  hardware.opengl.enable = true;
+}
