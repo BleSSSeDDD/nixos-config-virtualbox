eee510251ced6ffc3a5ced8761996c1e30672252
BleSSSeDDD <120226159+BleSSSeDDD@users.noreply.github.com>
2025-10-13 20:11:44 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,21 +2,40 @@
 
 {
   imports = [ ./hardware-configuration.nix ];
-  system.stateVersion = "25.05";
+  system.stateVersion = "25.05"; 
 
   boot.loader.grub = {
-   enable = true;
-   device = "/dev/sda";
+    enable = true;
+    device = "nodev";
+    useOSProber = true;
   };
+
+  boot.initrd.checkJournalingFS = false;
+  virtualisation.virtualbox.guest.enable = true;
+  
+  services.xserver.enable = false;
+  
+  services.displayManager = {
+    sddm.enable = true;
+    defaultSession = "plasmawayland";
+  };
+  
+  services.desktopManager.plasma6.enable = true;  # Plasma 6 с нативным Wayland
+
+  programs.plasma.enable = true;
+  security.polkit.enable = true;
 
   networking.hostName = "nixos-dev";
   networking.networkmanager.enable = true;
 
   users.users.dev = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" ];
+    description = "Developer";
+    extraGroups = [ "networkmanager" "wheel" "video" "input" ];
     initialPassword = "12345678";
   };
+
+  security.sudo.wheelNeedsPassword = false;
 
   environment.systemPackages = with pkgs; [
     inetutils iproute2 bind dnsutils
@@ -24,23 +43,21 @@
     file pciutils usbutils
   ];
 
-virtualisation.virtualbox.guest.enable = true;
-
-  services.xserver = {
+  hardware.opengl = {
     enable = true;
-    displayManager = {
-      lightdm.enable = true;
-      autoLogin = {
-        enable = true;
-        user = "dev";
-      };
-      defaultSession = "plasma";
-    };
-    desktopManager = {
-      plasma5.enable = true;
-    };
-    videoDrivers = [ "virtualbox" ];
+    driSupport = true;
   };
 
-  hardware.opengl.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+  };
+
+  services.printing.enable = true;
+  services.bluetooth.enable = true;
+  programs.bash.enable = true;
 }
